53fdcb7bb36d20fd5c54767b571eacb846ec9a97
Abolfazl <abolfazlmnf83@gmail.com>
2026-09-19 12:59:51 +0330

    configure prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,15 @@ import { PrismaService } from "./prisma.service";
 
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UsersController
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+    }),
   ],
-  providers: [
-    PrismaService,
-    UsersService
-  ],
+  controllers: [AppController, UsersController],
+  providers: [PrismaService, UsersService],
 })
 export class AppModule {}
