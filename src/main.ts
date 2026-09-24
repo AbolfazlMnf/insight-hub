d47d520337c87786e109e62046fd89942a971a9f
Abolfazl <abolfazlmnf83@gmail.com>
2026-09-24 15:29:34 +0330

    configure class validator and add password column

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.enableShutdownHooks();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   const config = new DocumentBuilder().setTitle(`Insight-Hub`).build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(`/documentation`, app, document);
