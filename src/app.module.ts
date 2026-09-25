58bfe1f4243c9682f3cbde7ecb5dfcf45eaf31f2
Abolfazl <abolfazlmnf83@gmail.com>
2026-09-25 13:20:35 +0330

    implement login endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -15,6 +16,10 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     UsersModule,
     AuthModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET!,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dtos/login.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UsersService,
     private readonly prismaService: PrismaService,
     private readonly jwtService: JwtService,
   ) {}
-  async login(body: LoginDto) {}
+  async login(body: LoginDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: body.email },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    const isPasswordMatch = await argon2.verify(user.password, body.password);
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    const payload = { id: user.id, role: user.role };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
 }
