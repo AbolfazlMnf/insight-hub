73a41a56d28892fe24d9550f893d284de2d4050a
Abolfazl <abolfazlmnf83@gmail.com>
2026-09-26 14:10:44 +0330

    add member to workspace by roles conditions

--- src/workspace/workspace.service.ts
+++ src/workspace/workspace.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateWorkspaceDto } from './dtos/create-workspace.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AddWorkspaceMemberDto } from './dtos/add-workspace-member.dto';
+import { WorkspaceRole } from 'src/generated/prisma/enums';
 
 @Injectable()
 export class WorkspaceService {
@@ -45,5 +51,45 @@ export class WorkspaceService {
     }
     return workspace;
   }
-  async addWorkspaceMember() {}
+  async addWorkspaceMember(
+    body: AddWorkspaceMemberDto,
+    currentUserId: string,
+    workspaceId: string,
+  ) {
+    const { targetUserId, role } = body;
+    const currentMember = await this.prismaService.workspaceMember.findUnique({
+      where: {
+        userId_workspaceId: { userId: currentUserId, workspaceId },
+      },
+    });
+    if (
+      !currentMember ||
+      (currentMember.role !== WorkspaceRole.ADMIN &&
+        currentMember.role !== WorkspaceRole.OWNER)
+    ) {
+      throw new ForbiddenException();
+    }
+    if (
+      currentMember.role === WorkspaceRole.ADMIN &&
+      role !== WorkspaceRole.MEMBER
+    ) {
+      throw new ForbiddenException(
+        'Admin can only add members with MEMBER role',
+      );
+    }
+    if (role === WorkspaceRole.OWNER) {
+      throw new ForbiddenException('Cannot assign OWNER role');
+    }
+    const newWorkspaceMember = await this.prismaService.workspaceMember.create({
+      data: {
+        userId: targetUserId,
+        role,
+        workspaceId,
+      },
+      include: {
+        workspace: true,
+      },
+    });
+    return newWorkspaceMember;
+  }
 }
